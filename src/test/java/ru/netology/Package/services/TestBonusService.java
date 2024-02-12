0942a517ae702683e4d0a75d6ff207f7a5868f14
Ромашко <romashko-bor@mail.ru>
2024-02-13 00:49:54 +0300

    csv commit

--- src/test/java/ru/netology/Package/services/TestBonusService.java
+++ src/test/java/ru/netology/Package/services/TestBonusService.java
@@ -2,32 +2,40 @@ package ru.netology.Package.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestBonusService {
 
-    @Test
-    public void oneTest() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "10000,3000,20000",
+//            "100000,60000,150000"
+//    })
+    @CsvFileSource(files = "src/test/resources/csv.bonus")
+    public void oneTest(int expected, int income, int expenses, int threshold) {
         BonusService service = new BonusService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3000, 20_000);
+       // int expected  = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
 
     }
 
-    @Test
-    public void twoTest() {
-        BonusService service = new BonusService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
+//    @Test
+//    public void twoTest() {
+//        BonusService service = new BonusService();
+//
+//        int expected = 2;
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//
+//
+//    }
 
 
 }
